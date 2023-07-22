5055bc835ca8c0078a5c137470a4078dc35fda36
whatody <106987822+whatody@users.noreply.github.com>
2023-07-22 13:41:52 +0900

    Initial Commit

---
+++ app/src/main/java/com/android/myapplication/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.android.myapplication
+
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var button10:Button
+        button10=findViewById<Button>(R.id.button10)
+
+        var button11:Button
+        button11=findViewById<Button>(R.id.button11)
+
+        var button12:Button
+        button12=findViewById<Button>(R.id.button12)
+
+        var button13:Button
+        button13=findViewById<Button>(R.id.button13)
+
+        var button14:Button
+        button14=findViewById<Button>(R.id.button14)
+
+        var button15:Button
+        button15=findViewById<Button>(R.id.button15)
+
+        var button16:Button
+        button16=findViewById<Button>(R.id.button16)
+
+
+        var button17:Button
+        button17=findViewById<Button>(R.id.button17)
+
+        var button18:Button
+        button18=findViewById<Button>(R.id.button18)
+
+        var button19:Button
+        button19=findViewById<Button>(R.id.button19)
+    }
+}
